dad95af78201f2a83f3242fdc368678aa96b2aa0
Scott D'Alessandro <sdaless22@gmail.com>
2016-04-12 11:27:16 -0400

    Update makingItModular.js

--- makingItModular.js
+++ makingItModular.js
@@ -1,5 +1,6 @@
 var mymodule = require('./mymodule');
-
+// the main idea is to use a module, which is demonstrated in your solution.  The tests specs are very vague for this problem
+// excellent job coming up with this solution!
 mymodule(process.argv[2], process.argv[3], function callback(err, filteredList){
 	if (err){
 			return callback(err);
